22d2206b6671f0a29390c33149cc42bd7fabf1e1
ATk <aroup@gmail.com>
2021-03-28 23:59:22 +0600

    first commit

---
+++ MVCLesson-09/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCLesson_09.MvcApplication" Language="C#" %>
---
+++ MVCLesson-09/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCLesson_09.Startup))]
+namespace MVCLesson_09
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
